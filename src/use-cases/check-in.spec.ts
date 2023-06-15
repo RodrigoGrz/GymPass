38d590c95f857ec1051e63483dee2a06830355da
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2023-06-15 07:57:14 -0300

    feat: check in use case

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,22 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-in'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: CheckInUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to get a user profile', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
